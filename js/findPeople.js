f255f3df817bcaf93a0875b63bcaa306f03baf9b
Richard Liu <richardliu7896@gmail.com>
2016-01-04 23:54:16 -0500

    findPeople basic skeleton down

--- js/findPeople.js
+++ js/findPeople.js
@@ -2,10 +2,6 @@ var LocationTime = require('../models/locationTimes.js');
 var User = require('../models/user.js');
 
 module.exports = function(from1, to1, date1, time1, callback){
-	console.log(from1);
-	console.log(to1);
-	console.log(date1);
-	console.log(time1);
 	LocationTime.findOne({'from': from1, 'to': to1, 'time': time1, 'date': date1}, 
 		function(err, locationTime){
 			if (err) {
@@ -16,29 +12,30 @@ module.exports = function(from1, to1, date1, time1, callback){
 				callback(false);
 			} else{
 				var userInfo = [];
+				var itemsProcessed = 0;
 				locationTime.usernames.forEach(function(username){
 					User.findOne({'username': username}, function(err, user){
-						console.log(user);
 						if (err){
 							console.log(err);
 							callback(false);
 						} else{
 							var obj = {
 								'username': username,
-								'firstName': user.firstName,
-								'lastName': user.lastName,
-								'email': user.email,
-								'phoneNum': user.phoneNum,
-								'fbUsername': user.fbUsername,
-								'fbName': user.fbName,
-								'prefEmail': user.prefEmail
+								'firstName': user.firstName? user.firstName:"",
+								'lastName': user.lastName? user.lastName:"",
+								'email': user.email? user.email:"",
+								'phoneNum': user.phoneNum? user.phoneNum:"",
+								'fbUsername': user.fbUsername? user.fbUsername:"",
+								'fbName': user.fbName? user.fbName:"",
+								'prefEmail': user.prefEmail? user.prefEmail:""
 							};
 							userInfo.push(obj);
+							if (++itemsProcessed == locationTime.usernames.length){
+								callback(true, userInfo);
+							}
 						}
 					});
 				});
-				console.log(userInfo);
-				callback(true, userInfo);
 			}
 		});
 }
